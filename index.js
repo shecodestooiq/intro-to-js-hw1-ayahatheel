d21ea865ffa2db2c722ce02ac7807fc01e17d314
Ayah Atheel <ayah.atheel@gmail.com>
2024-02-04 02:51:03 +0300

    all tasks done

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// Task 1: Variable Declaration and Logging
+var personName ="Sadin"
+var age = 1 
+var isStudent = false
+
+console.log("My name is" , personName);
+console.log("I'm", age, "years old.");
+console.log("am I a student?" , isStudent);
+
+
+
+//Task 2: Type Checking
+var variableToCheck = "I love broccoli"
+var typeofVar = typeof variableToCheck;
+console.log("This var is", typeof variableToCheck);
+
+
+
+
+// Task 3: String Concatenation
+var greeting = "Good Morning"
+var message = greeting + " " + personName
+console.log(message);
+
